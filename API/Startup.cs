5c098dd61112e7940dc64024447fa9ca4f881948
Bernardo Generoso <generoso.bernardoapple@gmail.com>
2021-12-01 18:26:05 +0000

    Configuring API

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,7 @@ namespace API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
         }
 
@@ -44,7 +44,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
