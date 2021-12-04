7440274d781c76c3121a0ce3ecf006a01dbbd6e8
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-12-04 10:27:56 +0000

    End section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
